5b7da7096d0fcea4545a9cc761a4a83169b30dd0
Johannes Teichrieb <isrvoid@gmail.com>
2019-09-03 14:35:02 +0200

    Add aspectnames specification

--- src/util/aspectnames.d
+++ src/util/aspectnames.d
@@ -10,6 +10,26 @@ import std.stdio : writeln; // FIXME remove
 
 import util.removecomments;
 
+/*
+ * specification:
+ *
+ * aspectnames sides with simplicity, incurring slight syntax restrictions:
+ * - don't comment out enum class Aspect definition
+ * - no comments or macros between enum and opening {
+ * - don't use preprocessor within enum class Aspect { } block (macros, include, conditional)
+ *      exception: specifying enum values is fine: foo = FIRST,
+ * - no commented out '{' or '}' between Aspect's 'enum' and ';'
+ *
+ * comments within { } are fine
+ * verifying the value of last element is a separate concern
+ *      (can't determine foo = VAL without preprocessor anyway)
+ * verifying '_end' enumerator is a separate concern, it is simply ignored:
+ * - no name is generated for _end
+ * - no check for missing _end -- it would fail to compile later anyway
+ * - _end not being the last element or assignment to it is up to developer:
+ *      e.g.: (_end, any = _end) or (any, _end = any)
+ */
+
 version (unittest) { } else
 void main()
 {
