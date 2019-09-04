6fc533095000747dd32d0929e5cb037f7563a47c
Johannes Teichrieb <isrvoid@gmail.com>
2019-09-04 19:01:18 +0200

    Add Parser
    TODO: handle enum value assignment

--- src/util/aspectnames.d
+++ src/util/aspectnames.d
@@ -4,11 +4,9 @@
  */
 module util.aspectnames;
 
-import std.regex : ctRegex, matchAll;
-import std.file : readText;
-import std.stdio : writeln; // FIXME remove
-
 import util.removecomments;
+
+@safe:
 
 /*
  * specification:
@@ -30,14 +28,173 @@ import util.removecomments;
  *      e.g.: (_end, any = _end) or (any, _end = any)
  */
 
+
 version (unittest) { } else
 void main()
 {
-    enum r = ctRegex!r"enum\s+class\s+(\w*Aspect)[^{]*\{([^}]*)\}\s*;";
-    auto text = readText("test/aspectnames/package.h");
-    foreach (c; matchAll(text, r))
+}
+
+struct Parser
+{
+    import std.regex : matchFirst;
+
+    struct Aspect
     {
-        writeln("match: ", c[1]);
-        writeln(c[2].removeCommentsReplaceStrings);
+        string name;
+        string[] elem;
+
+        string upToPost;
+        string post;
     }
+
+    this(string _input)
+    {
+        input = _input;
+        toParse = input;
+        matchNext();
+    }
+
+    bool empty() pure nothrow const
+    {
+        return cap.empty;
+    }
+
+    Aspect front() pure
+    {
+        return _front;
+    }
+
+    void popFront()
+        in (!empty)
+    {
+        matchNext();
+    }
+
+    private void matchNext()
+    {
+        cap = matchFirst(toParse, r);
+        if (!cap.empty)
+            convCap();
+
+        toParse = cap.post;
+    }
+
+    private void convCap() @trusted
+    {
+        import std.algorithm : map, filter;
+        import std.array : array, split;
+        import std.string : strip;
+        _front.name = cap[1];
+        _front.elem = cap[2]
+            .removeCommentsReplaceStrings
+            .strip
+            .split(',')
+            .map!strip
+            .filter!`a != "_end"`
+            .array;
+        const pPost = cap.post.ptr;
+        const pInput = input.ptr;
+        _front.upToPost = pInput[0 .. pPost - pInput];
+        _front.post = cap.post;
+    }
+
+    private
+    {
+        import std.regex : ctRegex, Captures;
+        enum aspectEnumNamePattern = r"enum\s+class\s+(\w*Aspect)";
+        enum aspectEnumPattern = aspectEnumNamePattern ~ r"[^{]*\{([^}]*)\}\s*;";
+        static const r = ctRegex!aspectEnumPattern;
+
+        string input;
+        string toParse;
+        Captures!string cap;
+        Aspect _front;
+    }
+}
+
+@("empty input") unittest
+{
+    auto parser = Parser("");
+    assert(parser.empty);
+}
+
+@("non empty input") unittest
+{
+    string input = "#include <cstdio>\n\nclass Foo { int i; };\n";
+    auto parser = Parser(input);
+    assert(parser.empty);
+}
+
+@("not aspect enum") unittest
+{
+    string input = "#include <cstddef>\n\nenum class Foo { one, two };\n";
+    auto parser = Parser(input);
+    assert(parser.empty);
+}
+
+@("empty aspect enum") unittest
+{
+    string input = "#include <cstdint>\n\nenum class Aspect { };\n// end\n";
+    auto parser = Parser(input);
+    assert(!parser.empty);
+    assert("Aspect" == parser.front.name);
+    assert(0 == parser.front.elem.length);
+    assert(input[0 .. $ - 8] == parser.front.upToPost);
+    assert("\n// end\n" == parser.front.post);
+}
+
+@("single element enum") unittest
+{
+    string input = "enum class FooAspect {foo};";
+    auto parser = Parser(input);
+    auto res = parser.front;
+    assert("FooAspect" == res.name);
+    assert(["foo"] == res.elem);
+    assert(input == res.upToPost);
+    assert("" == res.post);
+}
+
+@("popFront") unittest
+{
+    string input = "enum class Aspect {};";
+    auto parser = Parser(input);
+    assert(!parser.empty);
+    parser.popFront();
+    assert(parser.empty);
+}
+
+@("_end is ignored") unittest
+{
+    string input = "enum class Aspect { one, two, _end };";
+    auto parser = Parser(input);
+    assert(["one", "two"] == parser.front.elem);
+}
+
+@("two enums") unittest
+{
+    import std.algorithm : findSplitAfter;
+    string input = "enum class AAspect{ one,two,_end } ;
+                    enum class BAspect :unsigned int{ three, four, _end } ;
+    ";
+    auto parser = Parser(input);
+    auto res = parser.front;
+    assert("AAspect" == res.name);
+    assert(["one", "two"] == res.elem);
+
+    auto a = input.findSplitAfter(";");
+    assert(a[0] == res.upToPost);
+    assert(a[1] == res.post);
+
+    parser.popFront();
+    assert(!parser.empty);
+    res = parser.front;
+
+    assert("BAspect" == res.name);
+    assert(["three", "four"] == res.elem);
+
+    auto b = a[1].findSplitAfter(";");
+    assert(a[0] ~ b[0] == res.upToPost);
+    assert(b[1] == res.post);
+    parser.popFront();
+    assert(parser.empty);
 }
